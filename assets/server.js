fb688781a868c6617ecab7d8674e7ab5504cbd4b
Dallen Hatton <Dhatton2001@gmail.com>
2023-04-26 21:39:03 -0400

    final push

---
+++ assets/server.js
@@ -0,0 +1,35 @@
+// Import express package
+const express = require("express");
+const path = require("path");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+// Require the JSON file and assign it to a variable called `termData`
+const { request } = require("http");
+const termData = require("./terms.json");
+const PORT = process.env.PORT || 3001;
+
+// Initialize our app variable by setting it to the value of express()
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static("public"));
+
+const parsedNotes = Json.parse(Notes);
+console.info("parsed notes", parsedNotes);
+
+// routes for notes //
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Api Routes //
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
