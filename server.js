6386cb0d0ac30f2465e402556a9621342265c878
Dallen Hatton <Dhatton2001@gmail.com>
2023-04-27 21:48:17 -0400

    fixed routes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Import express package
 const express = require("express");
 const path = require("path");
-const htmlRoutes = require("./routes/assets/htmlRoutes");
-const apiRoutes = require("./routes/assets/apiRoutes");
+const htmlRoutes = require("./assets/routes/htmlRoutes");
+const apiRoutes = require("./assets/routes/apiRoutes");
 
 // Require the JSON file and assign it to a variable called `termData`
 const { request } = require("http");
